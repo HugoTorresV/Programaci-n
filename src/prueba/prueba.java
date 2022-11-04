527a43ed35f7b2969cfa2d3d7bdc82f03901dca5
dam129 <dam129@aula105-20.fp.17.es>
2022-11-04 17:25:58 +0100

    segundo commit

--- src/prueba/prueba.java
+++ src/prueba/prueba.java
@@ -1,5 +1,13 @@
 package prueba;
 
 public class prueba {
+ 
+	// TODO Auto-generated method stub{
+ {
+	// TODO Auto-generated method stub
 
+	 System.out.println("Esto es un cambio");
+	 System.out.println("Hola a todos");
 }
+}
+
